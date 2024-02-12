8cf60437b5107d0e00a119e6fe9792a050715d44
arpatil-dev <aryanrpatil2023@gmail.com>
2024-02-12 06:37:20 -0800

    asd

--- cyber-fraud/src/components/Navbar.jsx
+++ cyber-fraud/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
+import './Navbar.css'
 function Navbar(){
     return(
-        <div className="navbar">
-            
+        <div className="navbar d-flex justify-content-between align-items-center m-4">
+            <h2>Home</h2>
+            <div className="d-flex justify-content-space-between list-unstyled ">
+                <li className="mx-2">Presentation</li>
+                <li className="mx-2">Reporting</li>
+                <li className="mx-2">Contact</li>
+                <li className="mx-2">About</li>
+            </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
